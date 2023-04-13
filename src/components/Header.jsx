6638ba01f62ddd542cd2e0441dcb489189e1918f
Yaasir Cheekoory <yaasir1997@gmail.com>
2023-04-13 07:34:46 +0400

    completed navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,12 +4,8 @@ import Logo from '../assets/header/logo/logo.png';
 
 export default function Header() {
   return (
-    <section className="nav">
-      <div className="nav-container">
-        <div className="nav-wrapper">
-          <img src={Logo} alt="Logo of AirBnb" className='nav__logo' />
-        </div>
-      </div>
-    </section>
+    <nav>
+      <img src={Logo} alt="" />
+    </nav>
   )
 }
